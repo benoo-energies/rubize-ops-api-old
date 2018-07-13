19f49095f5b8632ccc2ac9f5298e25c94703c60f
Virginie JL <vjlockel@gmail.com>
2018-07-13 21:02:28 +0200

    Ajout destinataire mail commande entrepreneur

--- app/Http/Controllers/EntrepreneurOrderController.php
+++ app/Http/Controllers/EntrepreneurOrderController.php
@@ -39,7 +39,7 @@ class EntrepreneurOrderController extends Controller
             Mail::send('emails.entrepreneur-order', ['products' => $request->products, 'total' => $request['total'], "entrepreneur" => $entrepreneur], function ($m) {
                 $m->from('contact@benoo-energies.com', 'Benoo Energies');
     
-                $m->to(["akenfack@benoo-energies.com", "contact@benoo-energies.com"])->subject('Une nouvelle commande entrepreneur a été enregistrée');
+                $m->to(["akenfack@benoo-energies.com", "contact@benoo-energies.com", "mbordeleau@benoo-energies.com"])->subject('Une nouvelle commande entrepreneur a été enregistrée');
                 //$m->to("vjlockel@gmail.com")->subject('Une nouvelle commande entrepreneur a été enregistrée');
             });
 
